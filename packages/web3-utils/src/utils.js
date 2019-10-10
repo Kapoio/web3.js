2832b8d880d9079248f2ae72129074bada33ed50
ivanshukhov <45999338+ivanshukhov@users.noreply.github.com>
2019-10-10 15:22:04 +0200

    web3-utils: change address length

--- packages/web3-utils/src/utils.js
+++ packages/web3-utils/src/utils.js
@@ -85,10 +85,10 @@ var toTwosComplement = function (number) {
  */
 var isAddress = function (address) {
     // check if it has the basic requirements of an address
-    if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+    if (!/^(0x)?[0-9a-f]{128}$/i.test(address)) {
         return false;
         // If it's ALL lowercase or ALL upppercase
-    } else if (/^(0x|0X)?[0-9a-f]{40}$/.test(address) || /^(0x|0X)?[0-9A-F]{40}$/.test(address)) {
+    } else if (/^(0x|0X)?[0-9a-f]{128}$/.test(address) || /^(0x|0X)?[0-9A-F]{128}$/.test(address)) {
         return true;
         // Otherwise check each case
     } else {
@@ -110,7 +110,7 @@ var checkAddressChecksum = function (address) {
     address = address.replace(/^0x/i,'');
     var addressHash = sha3(address.toLowerCase()).replace(/^0x/i,'');
 
-    for (var i = 0; i < 40; i++ ) {
+    for (var i = 0; i < address.length; i++ ) {
         // the nth letter should be uppercase if the nth digit of casemap is 1
         if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) || (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
             return false;
